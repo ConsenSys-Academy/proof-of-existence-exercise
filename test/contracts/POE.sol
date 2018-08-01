8274f1d41a6bc1b954d10f6973f9bd6b1b15c40c
josh crites <critesjosh@gmail.com>
2018-08-01 17:12:00 -0400

    first commit

---
+++ test/contracts/POE.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.22;
+
+contract ProofOfExistence {
+      // state
+      bytes32 public proof;
+
+      // calculate and store the proof for a document
+      // *transactional function*
+      function notarize(string document) {
+        proof = proofFor(document);
+      }
+
+      // helper function to get a document's sha256
+      // *read-only function*
+      function proofFor(string document) constant returns (bytes32) {
+        return sha256(document);
+      }
+
+}
