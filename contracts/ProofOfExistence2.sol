3dfbc408f4af8b0032cc80a3048d5ed805a6a8c8
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2018-12-21 15:50:25 -0500

    Update ProofOfExistence2.sol

--- contracts/ProofOfExistence2.sol
+++ contracts/ProofOfExistence2.sol
@@ -7,33 +7,49 @@ contract ProofOfExistence2 {
 
   // store a proof of existence in the contract state
   // *transactional function*
-  function storeProof(bytes32 proof) public {
+  function storeProof(bytes32 proof) 
+    public 
+  {
     proofs.push(proof);
   }
 
   // calculate and store the proof for a document
   // *transactional function*
-  function notarize(string calldata document) external {
+  function notarize(string calldata document) 
+    external 
+  {
     bytes32 proof = proofFor(document);
     storeProof(proof);
   }
 
   // helper function to get a document's sha256
   // *read-only function*
-  function proofFor(string memory document) pure public returns (bytes32) {
+  function proofFor(string memory document) 
+    pure 
+    public 
+    returns (bytes32) 
+  {
     return sha256(abi.encodePacked(document));
   }
 
   // check if a document has been notarized
   // *read-only function*
-  function checkDocument(string memory document) public view returns (bool) {
+  function checkDocument(string memory document) 
+    public 
+    view 
+    returns (bool) 
+  {
     bytes32 proof = proofFor(document);
     return hasProof(proof);
   }
 
   // returns true if proof is stored
   // *read-only function*
-  function hasProof(bytes32 proof) internal view returns (bool) {
+  function hasProof(bytes32 proof) 
+    internal 
+    view 
+    returns (bool) 
+  {
     for (uint256 i = 0; i < proofs.length; i++) {
       if (proofs[i] == proof) {
         return true;
