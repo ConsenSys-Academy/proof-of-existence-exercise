7f3eee53a3d2c40e5d4e377d2701582e0f41fb01
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2018-12-21 15:51:40 -0500

    Update ProofOfExistence3.sol

--- contracts/ProofOfExistence3.sol
+++ contracts/ProofOfExistence3.sol
@@ -1,34 +1,49 @@
-pragma solidity ^0.4.22;
-
+pragma solidity ^0.5.0;
 
 contract ProofOfExistence3 {
 
   mapping (bytes32 => bool) private proofs;
   
   // store a proof of existence in the contract state
-  function storeProof(bytes32 proof) {
+  function storeProof(bytes32 proof) 
+    internal 
+  {
     proofs[proof] = true;
   }
   
   // calculate and store the proof for a document
-  function notarize(string document) {
-    var proof = proofFor(document);
+  function notarize(string calldata document) 
+    external 
+  { 
+    bytes32 proof = proofFor(document);
     storeProof(proof);
   }
   
   // helper function to get a document's sha256
-  function proofFor(string document) constant returns (bytes32) {
-    return sha256(document);
+  function proofFor(string memory document) 
+    pure 
+    public 
+    returns (bytes32) 
+  {
+    return keccak256(bytes(document));
   }
   
   // check if a document has been notarized
-  function checkDocument(string document) constant returns (bool) {
-    var proof = proofFor(document);
+  function checkDocument(string memory document) 
+    public 
+    view 
+    returns (bool) 
+  {
+    bytes32 proof = proofFor(document);
     return hasProof(proof);
   }
 
   // returns true if proof is stored
-  function hasProof(bytes32 proof) constant returns(bool) {
+  function hasProof(bytes32 proof) 
+    internal 
+    view 
+    returns(bool) 
+  {
     return proofs[proof];
   }
 }
